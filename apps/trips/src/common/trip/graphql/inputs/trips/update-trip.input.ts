232bc2f103f05c852b589141b9e34728c7745139
David Bujosa <davidbujosa@gmail.com>
2022-04-19 01:17:52 +0200

    fix: update trip input add validation input

--- apps/trips/src/common/trip/graphql/inputs/trips/update-trip.input.ts
+++ apps/trips/src/common/trip/graphql/inputs/trips/update-trip.input.ts
@@ -2,12 +2,19 @@ import { IUpdateEntity } from '@shared/data/interfaces/update-entity.interface';
 import { InputType, Field } from '@nestjs/graphql';
 import { GetEntityByIdInput } from '@shared/data/classes/get-entity-by-id.class';
 import { UpdateTripPayload } from './update-trip.payload';
+import * as joi from 'joi';
+import { ValidationInput } from '@shared/data/classes/validation-input.class';
 
 @InputType()
-export class UpdateTripInput implements IUpdateEntity {
+export class UpdateTripInput extends ValidationInput implements IUpdateEntity {
   @Field()
   where: GetEntityByIdInput;
 
   @Field()
   data: UpdateTripPayload;
+
+  public static validationSchema = joi.object<UpdateTripInput>({
+    where: GetEntityByIdInput.validationSchema.required(),
+    data: UpdateTripPayload.validationSchema.required()
+  });
 }
