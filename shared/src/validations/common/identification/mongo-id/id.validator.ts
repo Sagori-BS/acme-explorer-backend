1d90a2fbb34a93c67d87585a28dc8b65303c3b79
David Bujosa <davidbujosa@gmail.com>
2022-04-18 23:44:59 +0200

    feat: add mongo id validator with joi

--- shared/src/validations/common/identification/mongo-id/id.validator.ts
+++ shared/src/validations/common/identification/mongo-id/id.validator.ts
@@ -1,8 +1,8 @@
-import { IDatabaseValidator } from "@shared/data/interfaces/database-validator.interface";
-import { errorMessageBuilder } from "../../../error-message-builder";
-import { isValidObjectId, Types } from "mongoose";
-import * as joi from "joi";
-import { CustomHelpers } from "joi";
+import { IDatabaseValidator } from '@shared/data/interfaces/database-validator.interface';
+import { errorMessageBuilder } from '../../../error-message-builder';
+import { isValidObjectId, Types } from 'mongoose';
+import * as joi from 'joi';
+import { CustomHelpers } from 'joi';
 
 export const _validateId = (value: any): boolean => {
   try {
@@ -34,19 +34,27 @@ export const _validateId = (value: any): boolean => {
 
 export const validateId: IDatabaseValidator = {
   validator: _validateId,
-  message: errorMessageBuilder,
+  message: errorMessageBuilder
 };
 
 //Example custom validator
 const customIdValidator = (value: any, helpers: CustomHelpers) => {
   const condiction = _validateId(value);
   if (!condiction) {
-    return helpers.error("any.invalid");
+    return helpers.error('any.invalid');
   }
   return value;
 };
 
 export const validateIdWithJoi = joi
   .string()
-  .custom(customIdValidator, "ObjectId validator")
-  .messages({ "any.invalid": '"id" Invalid ObjectId' });
+  .custom(customIdValidator, 'ObjectId validator')
+  .messages({ 'any.invalid': '"id" Invalid ObjectId' });
+
+export const validateMongoIdWithJoi = (fieldName = 'field') =>
+  joi
+    .string()
+    .custom(customIdValidator, 'ObjectId validator')
+    .messages({
+      'any.invalid': `${fieldName}: received value is not a valid object id`
+    });
