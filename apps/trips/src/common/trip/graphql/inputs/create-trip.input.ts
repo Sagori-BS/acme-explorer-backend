04ffc211991a223a08ef19574cf52acf44e8456b
David Bujosa <davidbujosa@gmail.com>
2022-04-18 17:34:56 +0200

    feat: create stage inputs

--- apps/trips/src/common/trip/graphql/inputs/create-trip.input.ts
+++ apps/trips/src/common/trip/graphql/inputs/create-trip.input.ts
@@ -1,20 +1,42 @@
-import { InputType, Field, ID } from '@nestjs/graphql';
-import { TripState } from '../enums/trip-states.enum';
+import { Field, InputType } from '@nestjs/graphql';
+import { ValidationInput } from '@shared/data/classes/validation-input.class';
+import { validateUrlsWithJoi } from '@shared/validations/common/internet/urls/urls.validator';
+import { validateIsoDateWithJoi } from '@shared/validations/common/iso-date/iso-date.validator';
+import { validateDescriptionWithJoi } from '@shared/validations/common/strings/description/description.validator';
+import { validateTitleWithJoi } from '@shared/validations/common/strings/title/title.validator';
+import * as joi from 'joi';
+import { CreateStageInput } from './stages/create-stage.input';
 
 @InputType()
-export class CreateTripInput {
-  @Field(() => ID)
-  explorer: string;
+export class CreateTripInput extends ValidationInput {
+  @Field()
+  title: string;
 
-  @Field(() => ID)
-  trip: string;
+  @Field()
+  description: string;
 
-  @Field({ nullable: true })
-  reasonRejected?: string;
+  @Field(() => [String])
+  requirements: string[];
 
-  @Field(() => [String], { nullable: true })
-  comments?: string[];
+  @Field(() => [String])
+  pictures: string[];
 
-  @Field(() => TripState, { nullable: true })
-  state?: TripState;
+  @Field(() => [CreateStageInput])
+  stages: CreateStageInput[];
+
+  @Field()
+  startDate: string;
+
+  @Field()
+  endDate: string;
+
+  manager?: string;
+
+  public static validationSchema = joi.object<CreateTripInput>({
+    startDate: validateIsoDateWithJoi.required(),
+    endDate: validateIsoDateWithJoi.required(),
+    pictures: validateUrlsWithJoi.required(),
+    title: validateTitleWithJoi.required(),
+    description: validateDescriptionWithJoi.required()
+  });
 }
