541b788c47888b8cc4c75a8f5cc82e8bbb99b968
David Bujosa <davidbujosa@gmail.com>
2022-05-08 01:09:36 +0200

    feat: create check if trip is started

--- apps/trips/src/common/application/application.service.ts
+++ apps/trips/src/common/application/application.service.ts
@@ -11,6 +11,9 @@ import { ListApplications } from './graphql/types/list-applications.type';
 import { FilterInput } from '@shared/graphql/inputs/graphql-filter.input';
 import { UserRoles } from '@shared/auth/enums/user-roles.enum';
 import { CreateApplicationInput } from './graphql/inputs/create-application.input';
+import { TripState } from '../trip/graphql/enums/trip-states.enum';
+import { InvalidOperationException } from '@shared/errors/errors';
+import { Trip } from '../trip/database/trip.entity';
 
 @Injectable()
 export class ApplicationService extends Service<IApplicationServiceType> {
@@ -45,13 +48,15 @@ export class ApplicationService extends Service<IApplicationServiceType> {
   public async createEntity(
     createCustomApplicationInput: CreateCustomApplicationInput
   ): Promise<Application> {
-    if (!createCustomApplicationInput.manager) {
-      const trip = await this.tripService.getOneEntity({
-        id: createCustomApplicationInput.trip
-      });
+    const trip = await this.tripService.getOneEntity({
+      id: createCustomApplicationInput.trip
+    });
 
+    if (!createCustomApplicationInput.manager) {
       createCustomApplicationInput.manager = trip.manager.id;
     }
+
+    this.checkIfTripIsStarted(trip);
 
     return this.applicationRepository.createEntity(
       createCustomApplicationInput
@@ -65,6 +70,12 @@ export class ApplicationService extends Service<IApplicationServiceType> {
     const trip = await this.tripService.getOneEntity({
       id: createApplicationInput.trip
     });
+
+    if (trip.state !== TripState.ACTIVE) {
+      throw new InvalidOperationException('Trip is not active');
+    }
+
+    this.checkIfTripIsStarted(trip);
 
     const createCustomApplicationInput: CreateCustomApplicationInput = {
       ...createApplicationInput,
@@ -107,5 +118,17 @@ export class ApplicationService extends Service<IApplicationServiceType> {
     return this.applicationRepository.updateEntity(
       updateCustomApplicationInput
     );
+  }
+
+  private checkIfTripIsStarted(trip: Trip) {
+    // Check if trip start date and today the difference is less than 7 days
+    const tripStartDate = new Date(trip.startDate);
+    const today = new Date();
+    const diff = tripStartDate.getTime() - today.getTime();
+    const days = Math.ceil(diff / (1000 * 3600 * 24));
+
+    if (diff < 0 || days < 7) {
+      throw new InvalidOperationException('You can not apply for this trip');
+    }
   }
 }
