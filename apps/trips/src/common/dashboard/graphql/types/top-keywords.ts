de3482edb222fbefa8a0ad87f060ef17cca33c6f
David Bujosa <davidbujosa@gmail.com>
2022-05-16 19:18:42 +0200

    feat: make nullable

--- apps/trips/src/common/dashboard/graphql/types/top-keywords.ts
+++ apps/trips/src/common/dashboard/graphql/types/top-keywords.ts
@@ -2,7 +2,7 @@ import { Field, Float, ObjectType } from '@nestjs/graphql';
 
 @ObjectType()
 export class TopKeywords {
-  @Field()
+  @Field({ defaultValue: '' })
   keyword: string;
 
   @Field(() => Float, { defaultValue: 0 })
